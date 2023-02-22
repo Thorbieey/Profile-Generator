ca97ff453782f8e96223e39425bf05823927db5f
Tobi Olajide <tobiolajide887@yahoo.com>
2023-02-22 20:34:07 +0000

    added class Employee

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,41 @@
-// TODO: Write code to define and export the Employee class
+// Employee class
+class Employee {
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+
+    // methods which gets employee Name, Id, Email and Role 
+    getName() {
+        console.log(`Name: ${this.name}`); 
+        return this.name;
+    }
+    getId() {
+        console.log(`Id: ${this.id}`); 
+        return this.id;
+    }
+    getEmail() {
+        console.log(`Email: ${this.email}`); 
+        return this.email;
+    }
+    getRole() {
+        console.log("Role: Employee")
+        return "Employee"
+    }
+}
+
+// Create employee profile
+const tobi = new Employee("Tobi", 24, "tobi@yahoo.com");
+const pop = new Employee("Pop", 56, "pop@yahoo.com");
+
+pop.getName();
+pop.getId();
+pop.getEmail();
+pop.getRole();
+tobi.getName();
+tobi.getId();
+tobi.getEmail();
+tobi.getRole();
+
+module.exports = Employee;
